f62714f0d22ede67632f52813d783d00bae66002
Minghui <liminghui1993@gmail.com>
2023-06-24 03:16:20 +0800

    demo

--- components/routes/SentenceCatelogue/SentenceCatelogue.js
+++ components/routes/SentenceCatelogue/SentenceCatelogue.js
@@ -11,12 +11,19 @@ class SentenceCatelogue extends Component {
         this.state = { 
             selectedIndexes : [],
             indexChunkGroups : [],
-            editorEnable : false
+            editorEnable : false,
+            createSentence : false,
+            submitSentences : false,
+            sentenceAreaValue : ""
         };
     }
     
     // Lifecycle: Called whenever our component is created
     componentDidMount() {
+        let textarea = document.getElementById("sentence-textarea");
+        textarea.addEventListener("sl-change", (e) => {
+            this.setState({sentenceAreaValue : e.target.value});
+        })
     }
     
     // Lifecycle: Called just before our component will be destroyed
@@ -48,7 +55,7 @@ class SentenceCatelogue extends Component {
                                 </div>
                                 <div class="right">
                                     ${that.editorBtns(sentence)}
-                                    <sl-button onclick="${e => {that.goPracticeSentence(sentence)}}" class="practice-btn" variant="primary" outline>练习</sl-button>
+                                    <sl-button onclick="${e => {that.goPracticeSentence(sentence)}}" class="practice-btn" variant="primary">练习</sl-button>
                                 </div>
                             </li>`;
                     })
@@ -57,13 +64,58 @@ class SentenceCatelogue extends Component {
         `
     }
 
+    createNewSentence(e) {
+        if (!this.state.createSentence) {
+            this.setState({createSentence: true});
+        } else
+            this.setState({createSentence: false, submitSentences: false ,sentenceAreaValue: ""});
+    }
+
+    submitSentences(e) {
+        if (!this.state.sentenceAreaValue)
+            return;
+
+        if (!this.state.submitSentences )
+            this.setState({submitSentences : true});
+        else {
+            console.log("submitted");
+            this.setState({submitSentences : false});
+        }
+    }
+
+    displaySubmitSentences() {
+        if (!this.state.sentenceAreaValue)
+            return;
+        this.sentences = this.state.sentenceAreaValue.split("\n");
+        let that = this;
+        return html`
+            <ul>
+                ${
+                    this.sentences.map(function(sentence) {
+                        return html`
+                            <li>
+                                <div class="left">
+                                    <span class="content">${sentence}</span>
+                                    <div class="sub-content">长度：${that.difficultyOutput(sentence)}</div>
+                                </div>
+                                <div class="right">
+                                    <sl-button onclick="${e => console.log("deleted")}" class="practice-btn" variant="danger">删除</sl-button>
+                                </div>
+                            </li>`;
+                    })
+                }
+            </ul>
+        `
+    }
+
+    goEditSentenceText() {
+        alert("功能开发中");
+    }
+
     editorBtns(sentence) {
         if (this.state.editorEnable) {
             return html`
-                <!-- <a onclick="${() => alert("功能开发中")}">文本</a>
-                <a onclick="${() => alert("功能开发中")}">音频</a>
-                <a onclick="${e => {this.goSentenceCutter(sentence.sentenceId)}}">切割</a> -->
-                <sl-button onclick="${e => {alert("功能开发中")}}" 
+                <sl-button onclick="${e => {this.goEditSentenceText()}}" 
                            class="practice-btn" 
                            variant="primary" 
                            outline>
@@ -86,9 +138,13 @@ class SentenceCatelogue extends Component {
     }
 
     difficultyOutput(englishSen) {
-        let senLen = englishSen.split(" ").length;
-        let levelIndex = 0;
-        return senLen;
+        if (englishSen) {
+            let senLen = englishSen.split(" ").length;
+            let levelIndex = 0;
+            return senLen;
+        }
+        else
+            return "";
         // let colorList = ['c-d69824', 'c-d64224', 'c-0c249c', 
         //                  'c-a31297', 'c-0c7f9c', 'c-5b12a3', 'c-339c0c'];
         // let difficultyWord = ['level 1', 'level 2', 'level 3', 'level 4', 'level 5', 'level 6', 'level 7'];
@@ -154,9 +210,8 @@ class SentenceCatelogue extends Component {
                                     outline>${window.location.href} <sl-icon name="link-45deg"></sl-icon></sl-button>
                         </sl-tooltip>
                     </div>
-                    
                 </div>
-                <div id="sentence-catelogue" style="display:flex; flex-direction:column; margin:3%; padding:3%;">
+                <div id="sentence-catelogue">
                     <div class="top">
                         <sl-breadcrumb class="left">
                             <sl-breadcrumb-item>
@@ -166,14 +221,37 @@ class SentenceCatelogue extends Component {
                             <sl-breadcrumb-item>PTE题库</sl-breadcrumb-item>
                         </sl-breadcrumb>
                         <sl-button onclick="${e => {this.enableEditor()}}"
-                                class="right" 
-                                variant="primary" 
-                                outline>
+                                   class="right" 
+                                   variant="primary" 
+                                   style="visibility:${this.state.createSentence ? "hidden" : ""}"
+                                   outline>
                             ${this.state.editorEnable?"关闭":"开启"}编辑模式
                         </sl-button>
+                        <sl-button onclick="${e => {this.createNewSentence(e)}}"
+                                   class="right" 
+                                   variant="primary" 
+                                   outline>
+                            ${this.state.createSentence ? "关闭" : "导入新句子"}
+                        </sl-button>
                     </div>
-                    <div class="item-list" id="item-list">
+                    <div class="item-list" id="item-list" style="display:${this.state.createSentence ? "none" : ""}">
                         ${this.displaySentences()} 
+                    </div> 
+                    <div class="create-sentence" id="create-sentence" style="display:${this.state.createSentence ? "" : "none"}">
+                        <sl-textarea id="sentence-textarea"
+                                     value="${this.state.sentenceAreaValue}" 
+                                     placeholder="可上传多个句子，每句另起一行" 
+                                     style="display:${this.state.submitSentences ? "none" : ""}"
+                                     filled>
+                        </sl-textarea>
+                        <div class="item-list" style="display:${this.state.submitSentences ? "" : "none"}">
+                            ${this.displaySubmitSentences()} 
+                        </div> 
+                        <sl-button onclick="${e => {this.submitSentences(e)}}"
+                                   class="upload-btn" 
+                                   variant="primary">
+                            ${this.state.submitSentences ? "上传" : "提交"}
+                        </sl-button>
                     </div>
                 </div>
             </div>
---
+++ js/fauna-api.js
@@ -0,0 +1,351 @@
+const faunadb = window.faunadb;
+const q = faunadb.query;
+const client = new faunadb.Client({
+    secret: 'fnAEuSVyeiACVDsAHVqZ11gAA3QQcWTrtpu6MaFO',
+    domain: 'db.fauna.com', // Adjust if you are using Region Groups
+});
+
+const loginClient = new faunadb.Client({
+    secret: 'fnAEuciu5OACSced61iFoh5Db4PqimB-cRcd3RPF',
+    domain: 'db.fauna.com',
+})
+
+function setupFauna() {
+    client.query(
+        q.ToDate('2018-06-06')
+    )
+    .then(function (res) { console.log('Result:', res) })
+    .catch(function (err) { console.log('Error:', err) })
+}
+
+async function faunaAddSentence(sentence) {
+    let newSentence = JSON.parse(JSON.stringify(sentence));
+    newSentence.start = q.Time(newSentence.start);
+    newSentence.end = q.Time(newSentence.end);
+    return new Promise(resolve => {
+        client.query(
+            q.Create(
+                q.Collection('pte-listening-tool'),
+                { data: newSentence }
+            )
+        )
+        .then(ret => {
+            return resolve(ret);
+        })
+        .catch(err => {
+            return resolve(err);
+        })
+    })
+}
+
+async function faunaAddEvent(event) {
+    let newEvent = JSON.parse(JSON.stringify(event));
+    newEvent.start = q.Time(newEvent.start);
+    newEvent.end = q.Time(newEvent.end);
+    return new Promise(resolve => {
+        client.query(
+            q.Create(
+                q.Collection('weekly-schedule'),
+                { data: newEvent }
+            )
+        )
+        .then(ret => {
+            return resolve(ret);
+        })
+        .catch(err => {
+            return resolve(err);
+        })
+    })
+}
+
+async function faunaDeleteAdminEvent(startISOstr, endISOstr) {
+    return new Promise(resolve => {
+        client.query(
+            q.Map(
+                q.Paginate(
+                    q.Range(
+                        q.Match(q.Index('eventstart-outline')), 
+                        q.Time(startISOstr), 
+                        q.Time(endISOstr)
+                    ),
+                    { size: 1000 }
+                ),
+                q.Lambda(
+                    ["start", "ref"],
+                    q.Delete(q.Var("ref"))
+                )
+            )
+        )
+        .then(function (res) { 
+            return resolve(res.data) 
+        })
+        .catch(function (err) { return resolve({}) })
+    });
+}
+
+async function faunaBookEvent(event) {
+    event.start = q.Time(event.start);
+    event.end = q.Time(event.end);
+    return new Promise(resolve => {
+        client.query(
+            q.Create(
+                q.Collection('events-booked'),
+                { data: event }
+            )
+        )
+        .then(ret => {
+            return resolve(ret);
+        })
+        .catch(err => {
+            return resolve(err);
+        })
+    })
+}
+
+async function faunaAddEvents(events, prevEventsLen, startISOstr, endISOstr) {
+    let newEvents = JSON.parse(JSON.stringify(events));
+    newEvents.map(event => {
+        event.start = q.Time(event.start);
+        event.end = q.Time(event.end);
+    })
+    return new Promise(resolve => {
+        faunaDeleteEventsCurrentMonth(startISOstr, endISOstr, prevEventsLen)
+        .then(res => {
+            client.query(
+                q.Map(
+                    newEvents,
+                    q.Lambda(
+                        'data',
+                        q.Create(
+                            q.Collection('weekly-schedule'),
+                            { data: q.Var('data') },
+                        )
+                    )
+                )
+            )
+            .then(ret => {
+                return resolve(ret);
+            })
+            .catch(err => {
+                return resolve(err);
+            })
+            
+        })
+    })
+}
+
+async function faunaDeleteEventsCurrentMonth(startISOstr, endISOstr, prevEventsLen) {
+    return new Promise(resolve => {
+        client.query(
+            q.Map(
+                q.Paginate(
+                    q.Range(
+                        q.Match(q.Index('eventstart-outline')), 
+                        q.Time(startISOstr), 
+                        q.Time(endISOstr)
+                    ),
+                    { size: 1000 }
+                ),
+                q.Lambda(
+                    ["start", "ref"],
+                    q.Delete(q.Var("ref"))
+                )
+            )
+        )
+        .then(function (res) { 
+            return resolve(res.data);
+        })
+        .catch(function (err) { return resolve({}) })
+    })
+}
+
+// 获取一个月之内的课程信息
+async function faunaGetEventsCurrentMonth(startISOstr, endISOstr) {
+    return new Promise(resolve => {
+        client.query(
+            q.Map(
+                q.Paginate(
+                    q.Range(
+                        q.Match(q.Index('eventstart-outline')), 
+                        q.Time(startISOstr), 
+                        q.Time(endISOstr)
+                    ),
+                    { size: 1000 }
+                ),
+                q.Lambda(
+                    ["start", "ref"],
+                    q.Get(q.Var("ref"))
+                )
+            )
+        )
+        .then(function (res) { 
+            return resolve(res.data) 
+        })
+        .catch(function (err) { return resolve({}) })
+    })
+}
+
+// 获取一个月之内,已预定的课程信息
+async function faunaGetBookedEventsCurrentMonth(startISOstr, endISOstr) {
+    return new Promise(resolve => {
+        client.query(
+            q.Map(
+                q.Paginate(
+                    q.Range(
+                        q.Match(q.Index('eventstart-booked')), 
+                        q.Time(startISOstr), 
+                        q.Time(endISOstr)
+                    ),
+                    { size: 1000 }
+                ),
+                q.Lambda(
+                    ["start", "ref"],
+                    q.Get(q.Var("ref"))
+                )
+            )
+        )
+        .then(function (res) { return resolve(res.data) })
+        .catch(function (err) { return resolve({}) })
+    })
+}
+
+async function faunaAddTutor(tutor) {
+    let res = await client.query(
+        q.Create(
+            q.Collection('tutor'),
+            { data: tutor }
+        )
+    )
+    return res;
+}
+
+async function faunaAddStudent(student) {
+    return new Promise(resolve => {
+        client.query(
+            q.Create(
+                q.Collection('student'),
+                { data: student }
+            )
+        )
+        .then(function (res) { return resolve(res.data) })
+        .catch(function (err) { return resolve({}) })
+    })
+}
+
+async function saveAdminSetting(slotMinMax, unavailableTS, hiddenD) {
+    let newSetting =  {
+          "slotMinMax": slotMinMax,
+          "unavailableTimeSlots": unavailableTS,
+          "hiddenDays": hiddenD
+    };
+    return new Promise(resolve => {
+        client.query(
+            q.Update(
+                q.Ref(q.Collection("admin-setting"), "340582055893008978"),
+                {
+                    data: newSetting
+                },
+            )
+        )
+        .then((ret) => {
+            return resolve(ret);
+        })
+        .catch((err) => console.error(
+            'Error: [%s] %s: %s',
+            err.name,
+            err.message,
+            err.errors()[0].description,
+            alert("网络异常，请重新加载页面")
+        ))
+    })
+}
+
+async function getAdminSetting() {
+    return new Promise(resolve => {
+        client.query(
+            q.Get(
+                q.Ref(q.Collection("admin-setting"), "340582055893008978")
+            )
+        )
+        .then((ret) => {
+            return resolve(ret);
+        })
+        .catch((err) => console.error(
+            'Error: [%s] %s: %s',
+            err.name,
+            err.message,
+            err.errors()[0].description,
+        ))
+    })
+}
+
+async function faunaGetEvents() {
+    let events = await client.query(
+        q.Map(
+            q.Paginate(
+                q.Documents(
+                    q.Collection('weekly-schedule'),
+                    { size: 1000 }
+                )
+            ),
+            q.Lambda(x => q.Get(x))
+        )
+    )
+    return events.data;
+}
+
+async function faunaGetTutors() {
+    let events = await client.query(
+        q.Map(
+            q.Paginate(q.Documents(q.Collection('tutor'))),
+            q.Lambda(x => q.Get(x))
+        )
+    )
+    return events.data;
+}
+
+async function faunaDeleteEvent(refid) {
+    let res = await client.query(
+        q.Delete(q.Ref(q.Collection('events-booked'), refid))
+    )
+    return res;
+}
+
+async function faunaFindItemByValue(collection, key, value) {
+    return new Promise(resolve => {
+        client.query(
+            q.CreateIndex({
+                name: "search-by-" + key,
+                source: q.Collection(collection),
+                terms: [ { field: ["data", key] } ],
+                unique: true // id are unique
+            })
+        )
+        .then((ret) => {
+            client.query(
+                q.Get(
+                    q.Match(q.Index('search-by-' + key), value)
+                )
+            )
+            .then((ret) => {
+                return resolve(ret);
+            })
+            .catch(ret => {
+                return resolve({})
+            })
+        })
+        .catch((err) => {
+            client.query(
+                q.Get(
+                    q.Match(q.Index('search-by-' + key), value)
+                )
+            )
+            .then((ret) => {
+                return resolve(ret);
+            })
+            .catch(ret => {
+                return resolve({})
+            })
+        })
+    })
+}
